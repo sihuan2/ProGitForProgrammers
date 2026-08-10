384753699cd0a76fd13417076a5cf82a86bc754c
sihuan2 <sihuan2@gmail.com>
2026-08-10 23:38:45 +0800

    Add Informative line.

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace ProGitForProgrammers
+{
+	class program
+	{
+		static void Main(String[] args)
+		{
+
+			Console.WriteLine ("Hello, World!");
+			Console.WriteLine ("I just added this in Visual Studio.");
+		}
+	}
+}
