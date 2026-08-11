22b15c50a153b803028c2d0bdc19bef82a3408cd
sihuan2 <sihuan2@gmail.com>
2026-08-11 21:57:38 +0800

    my message

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -7,6 +7,7 @@
 
 			Console.WriteLine ("Hello, World!");
 			Console.WriteLine ("I just added this in Visual Studio.");
-		}
+            Console.WriteLine ("This line add to show the staging area.");
+        }
 	}
 }
